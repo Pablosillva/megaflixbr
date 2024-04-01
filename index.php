fafc1d41352c94e87726899e78101b52a5a9b639
Pablo Silva <98847763+Pablosillva@users.noreply.github.com>
2024-03-31 21:10:51 -0300

    megaflixbr

---
+++ index.php
@@ -0,0 +1,293 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="assets/css/style.css">
+    <link rel="stylesheet" href="assets/css/swiper-bundle.min.css">
+    <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
+
+
+    <title>MegaflixBR</title>
+</head>
+
+<body>
+    <!-- Inicio Cabeçalho do site -->
+    <header>
+        <div class="nav container">
+            <a href="index.php" class="logo" title="MegaflixBR">
+                Megaflix<span>BR</span>
+            </a>
+            <div class="busca-site">
+                <input type="search" class="busca-input" placeholder="Buscar Filmes">
+                <i class="bx bx-search"></i>
+            </div>
+            <a href="" class="user">
+                <img src="assets/img//p1.png" alt="Maykon Silveira" class="user-img">
+            </a>
+            <div class="navbar">
+
+                <a href="#home" class="nav-link">
+                    <i class="bx bx-home"></i>
+                    <span class="nav-link-title">Home</span>
+                </a>
+                <a href="#popular" class="nav-link">
+                    <i class="bx bxs-hot"></i>
+                    <span class="nav-link-title">Em alta</span>
+                </a>
+                <a href="#filmes" class="nav-link">
+                    <i class="bx bx-compass"></i>
+                    <span class="nav-link-title">Explore</span>
+                </a>
+                <a href="#filmes" class="nav-link">
+                    <i class="bx bx-tv"></i>
+                    <span class="nav-link-title">Filmes</span>
+                </a>
+                <a href="#home" class="nav-link">
+                    <i class="bx bx-heart"></i>
+                    <span class="nav-link-title">Favoritos</span>
+                </a>
+
+            </div>
+        </div>
+    </header>
+    <!-- Fim Cabeçalho do site -->
+
+    <!-- Inicio Banner Destaque -->
+
+    <section class="banner container">
+
+        <img src="assets/img/home-background.png" alt="" class="banner-img">
+        <div class="banner-texto">
+            <h1 class="banner-titulo">Super Mário 2 <br> o filme</h1>
+            <p>Lançamento em Setembro</p>
+            <a href="#" class="ver-btn">
+                <i class="bx bx-right-arrow"></i>
+                <span>Veja o Trailer</span>
+            </a>
+        </div>
+
+    </section>
+    <!-- Fim Banner Destaque -->
+
+    <!-- Inicio Topo Em Alta  -->
+    <section class="popular container" id="popular">
+        <div class="topFilmes">
+            <h2 class="topTitulo">Em Alta</h2>
+            <div class="swiper-btn">
+                <div class="swiper-button-prev"></div>
+                <div class="swiper-button-next"></div>
+            </div>
+        </div>
+
+        <!-- Fim Topo Em Alta  -->
+
+        <!-- Box slide em alta -->
+        <div class="popular-content swiper">
+            <div class="swiper-wrapper">
+                <div class="swiper-slide">
+                    <div class="filme-box">
+                        <img src="assets/img/filme-1.jpg" alt="" class="filme-box-img">
+                        <div class="box-titulo">
+                            <h2 class="titulo-filme">Genesis: o ínicio de tudo!</h2>
+                            <span class="categoria-filme">Gospel</span>
+                            <a href="ver-filme.php" class="ver-btn play-btn">
+                                <i class="bx bx-right-arrow"></i>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+                <div class="swiper-slide">
+                    <div class="filme-box">
+                        <img src="assets/img/filme-2.jpg" alt="" class="filme-box-img">
+                        <div class="box-titulo">
+                            <h2 class="titulo-filme">Gênesis: o ínicio de tudo!</h2>
+                            <span class="categoria-filme">Gospel</span>
+                            <a href="ver-filme.php" class="ver-btn play-btn">
+                                <i class="bx bx-right-arrow"></i>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+                <div class="swiper-slide">
+                    <div class="filme-box">
+                        <img src="assets/img/filme-3.jpg" alt="" class="filme-box-img">
+                        <div class="box-titulo">
+                            <h2 class="titulo-filme">Genesis: o ínicio de tudo!</h2>
+                            <span class="categoria-filme">Gospel</span>
+                            <a href="ver-filme.php" class="ver-btn play-btn">
+                                <i class="bx bx-right-arrow"></i>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+                <div class="swiper-slide">
+                    <div class="filme-box">
+                        <img src="assets/img/filme-4.jpg" alt="" class="filme-box-img">
+                        <div class="box-titulo">
+                            <h2 class="titulo-filme">Genesis: o ínicio de tudo!</h2>
+                            <span class="categoria-filme">Gospel</span>
+                            <a href="ver-filme.php" class="ver-btn play-btn">
+                                <i class="bx bx-right-arrow"></i>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+                <div class="swiper-slide">
+                    <div class="filme-box">
+                        <img src="assets/img/filme-5.jpg" alt="" class="filme-box-img">
+                        <div class="box-titulo">
+                            <h2 class="titulo-filme">Genesis: o ínicio de tudo!</h2>
+                            <span class="categoria-filme">Gospel</span>
+                            <a href="ver-filme.php" class="ver-btn play-btn">
+                                <i class="bx bx-right-arrow"></i>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+                <div class="swiper-slide">
+                    <div class="filme-box">
+                        <img src="assets/img/filme-6.jpg" alt="" class="filme-box-img">
+                        <div class="box-titulo">
+                            <h2 class="titulo-filme">Genesis: o ínicio de tudo!</h2>
+                            <span class="categoria-filme">Gospel</span>
+                            <a href="ver-filme.php" class="ver-btn play-btn">
+                                <i class="bx bx-right-arrow"></i>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+                <div class="swiper-slide">
+                    <div class="filme-box">
+                        <img src="assets/img/filme-7.jpg" alt="" class="filme-box-img">
+                        <div class="box-titulo">
+                            <h2 class="titulo-filme">Genesis: o ínicio de tudo!</h2>
+                            <span class="categoria-filme">Gospel</span>
+                            <a href="ver-filme.php" class="ver-btn play-btn">
+                                <i class="bx bx-right-arrow"></i>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+                <div class="swiper-slide">
+                    <div class="filme-box">
+                        <img src="assets/img/filme-8.jpg" alt="" class="filme-box-img">
+                        <div class="box-titulo">
+                            <h2 class="titulo-filme">Genesis: o ínicio de tudo!</h2>
+                            <span class="categoria-filme">Gospel</span>
+                            <a href="ver-filme.php" class="ver-btn play-btn">
+                                <i class="bx bx-right-arrow"></i>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!-- Fim Box slide em alta -->
+
+    <!-- Inicio Filmes -->
+    <section class="filmes container" id="movies">
+        <div class="topFilmes">
+            <h2 class="topTitulo">Filmes e Shows</h2>
+        </div>
+        <div class="filmes-content">
+            <div class="filme-box">
+                <img src="assets/img/filme-8.jpg" alt="" class="filme-box-img">
+                <div class="box-titulo">
+                    <h2 class="titulo-filme">Genesis: o ínicio de tudo!</h2>
+                    <span class="categoria-filme">Gospel</span>
+                    <a href="ver-filme.php" class="ver-btn play-btn">
+                        <i class="bx bx-right-arrow"></i>
+                    </a>
+                </div>
+            </div>
+            <div class="filme-box">
+                <img src="assets/img/filme-9.jpg" alt="" class="filme-box-img">
+                <div class="box-titulo">
+                    <h2 class="titulo-filme">Genesis: o ínicio de tudo!</h2>
+                    <span class="categoria-filme">Gospel</span>
+                    <a href="ver-filme.php" class="ver-btn play-btn">
+                        <i class="bx bx-right-arrow"></i>
+                    </a>
+                </div>
+            </div>
+            <div class="filme-box">
+                <img src="assets/img/filme-10.jpg" alt="" class="filme-box-img">
+                <div class="box-titulo">
+                    <h2 class="titulo-filme">Genesis: o ínicio de tudo!</h2>
+                    <span class="categoria-filme">Gospel</span>
+                    <a href="ver-filme.php" class="ver-btn play-btn">
+                        <i class="bx bx-right-arrow"></i>
+                    </a>
+                </div>
+            </div>
+            <div class="filme-box">
+                <img src="assets/img/filme-11.jpg" alt="" class="filme-box-img">
+                <div class="box-titulo">
+                    <h2 class="titulo-filme">Genesis: o ínicio de tudo!</h2>
+                    <span class="categoria-filme">Gospel</span>
+                    <a href="ver-filme.php" class="ver-btn play-btn">
+                        <i class="bx bx-right-arrow"></i>
+                    </a>
+                </div>
+            </div>
+            <div class="filme-box">
+                <img src="assets/img/filme-12.jpg" alt="" class="filme-box-img">
+                <div class="box-titulo">
+                    <h2 class="titulo-filme">Genesis: o ínicio de tudo!</h2>
+                    <span class="categoria-filme">Gospel</span>
+                    <a href="ver-filme.php" class="ver-btn play-btn">
+                        <i class="bx bx-right-arrow"></i>
+                    </a>
+                </div>
+            </div>
+            <div class="filme-box">
+                <img src="assets/img/filme-13.jpg" alt="" class="filme-box-img">
+                <div class="box-titulo">
+                    <h2 class="titulo-filme">Genesis: o ínicio de tudo!</h2>
+                    <span class="categoria-filme">Gospel</span>
+                    <a href="ver-filme.php" class="ver-btn play-btn">
+                        <i class="bx bx-right-arrow"></i>
+                    </a>
+                </div>
+            </div>
+            <div class="filme-box">
+                <img src="assets/img/filme-14.jpg" alt="" class="filme-box-img">
+                <div class="box-titulo">
+                    <h2 class="titulo-filme">Genesis: o ínicio de tudo!</h2>
+                    <span class="categoria-filme">Gospel</span>
+                    <a href="ver-filme.php" class="ver-btn play-btn">
+                        <i class="bx bx-right-arrow"></i>
+                    </a>
+                </div>
+            </div>
+            <div class="filme-box">
+                <img src="assets/img/filme-15.jpg" alt="" class="filme-box-img">
+                <div class="box-titulo">
+                    <h2 class="titulo-filme">Genesis: o ínicio de tudo!</h2>
+                    <span class="categoria-filme">Gospel</span>
+                    <a href="ver-filme.php" class="ver-btn play-btn">
+                        <i class="bx bx-right-arrow"></i>
+                    </a>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!-- Fim Filmes -->
+    <!-- Inicio Proxima pagina -->
+    <div class="proxima-pagina">
+        <a href="" class="proxima-btn">Próxima Página</a>
+    </div>
+    <!-- Fim Proxima pagina -->
+    <!-- Inicio Rodapé -->
+    <footer class="direitos">
+        <p>&#169; Todos os direitos reservados a Pablo Silva.</p>
+    </footer>
+    <!-- Fim Rodapé -->
+    <script src="assets/js/swiper-bundle.min.js"></script>
+    <script src="assets/js/scripts.js"></script>
+</body>
+
+</html>
