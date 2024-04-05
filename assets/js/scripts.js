45c6d6aafd6afba20f7111e471a0d0b000d6e39a
Pablo Silva <98847763+Pablosillva@users.noreply.github.com>
2024-04-04 21:26:09 -0300

    Update scripts.js

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,3 +1,4 @@
+// Swiper
 var swiper = new Swiper(".popular-content", {
     slidesPerView: 1,
     spaceBetween: 10,
@@ -6,35 +7,50 @@ var swiper = new Swiper(".popular-content", {
         disableOnInteraction: false,
     },
     pagination: {
-      el: ".swiper-pagination",
-      clickable: true,
-
+        el: ".swiper-pagination",
+        clickable: true,
     },
     navigation: {
-     nextEl: ".swiper-button-next",
-     prevEl: ".swiper-button-prev",
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
     },
     breakpoints:{
-        280: {
+        280:{
             slidesPerView: 1,
             spaceBetween: 10,
         },
-        320: {
+        320:{
             slidesPerView: 2,
             spaceBetween: 10,
         },
-        510: {
+        510:{
             slidesPerView: 2,
             spaceBetween: 10,
         },
-        758: {
+        758:{
             slidesPerView: 3,
             spaceBetween: 15,
         },
-        900: {
+        900:{
             slidesPerView: 4,
             spaceBetween: 20,
         },
     },
-
 });
+
+// Show Video
+let playButton = document.querySelector('.play-filme');
+let video = document.querySelector('.filme-container');
+let myvideo = document.querySelector('#myvideo');
+let closebtn = document.querySelector('.close-video');
+
+playButton.onclick = () =>{
+    video.classList.add("show-video");
+    myvideo.play();
+
+};
+closebtn.onclick = () =>{
+    video.classList.remove("show-video");
+    myvideo.pause();
+
+};
