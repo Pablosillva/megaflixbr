dec73318d1ab76539bd1ec2aeb17a7fdf1b66c67
Pablo Silva <98847763+Pablosillva@users.noreply.github.com>
2024-11-04 00:11:06 -0300

    Update and rename playpage.html to playpage.php

--- playpage.php
+++ playpage.php
@@ -14,9 +14,7 @@
 </head>
 
 <body>
-    <script language="php">
     <?php include('popcash.php'); ?>
-    </script>
     <header>
         <div class="nav container">
             <a href="index.html" class="logo">
